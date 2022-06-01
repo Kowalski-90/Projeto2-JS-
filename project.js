9861c2ca8ba9eecc96a51f1f66735f425a4ca7cd
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-06-01 08:46:22 -0300

    Tentativa1

---
+++ project.js
@@ -0,0 +1,7 @@
+let container = document.createElement("div");
+container.className = "container";
+document.body.appendChild(container);
+
+let item = document.createElement("div");
+item.className = "item";
+container.appendChild(item);
